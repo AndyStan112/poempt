0bb1ae6539d65e3c5ff05d7f81783ffda9bcaed4
RaduTek <radutek13@gmail.com>
2023-04-01 01:18:58 +0300

    API

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { Configuration, OpenAIApi } from 'openai';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
   apiKey: process.env.GPT,
@@ -13,11 +13,11 @@ const getResult = () => {};
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse,
+  res: NextApiResponse
 ) {
   try {
     const completion = await openai.createCompletion({
-      model: 'text-davinci-003',
+      model: "text-davinci-003",
       prompt: req.body.text,
       temperature: 0.7,
       top_p: 1,
@@ -26,9 +26,11 @@ export default async function handler(
       max_tokens: 32,
     });
     console.log(completion.data.choices[0]);
-    res.status(200).send({ name: completion.data.choices[0] });
+    res
+      .status(200)
+      .send({ result: "ok", poemText: completion.data.choices[0].text });
   } catch (error) {
     console.log(error);
-    res.status(500).send({ name: 'Internal server error' });
+    res.status(500).send({ result: "error" });
   }
 }
