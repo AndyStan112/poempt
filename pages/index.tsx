4632a9b787af75d41282d71c06d3ad1b80af2588
RaduTek <radutek13@gmail.com>
2023-04-01 22:13:20 +0300

    Update responsive hero banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next";
-import { Alert, Button } from "flowbite-react";
+import { Button } from "flowbite-react";
 import MainNavbar from "../components/navbar";
 import MainPage from "../components/page";
 import Waves from "../components/waves";
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
       <MainNavbar />
       <MainPage>
         <HeroBanner>
-          <div className="flex flex-row h-[450px]">
+          <div className="flex flex-col md:flex-row gap-8 h-[450px]">
             <div
               className="flex-1 flex items-center justify-center bg-contain bg-no-repeat bg-center"
               style={{ backgroundImage: "url(hero_bg.png)" }}
@@ -26,15 +26,15 @@ const Home: NextPage = () => {
                 className="w-1/2 mt-14"
               />
             </div>
-            <div className="flex-1 flex flex-col gap-4 justify-center">
-              <h1 className="text-white text-5xl drop-shadow-lg">
+            <div className="flex-1 flex flex-col gap-4 justify-center text-center md:text-left">
+              <h1 className="text-white text-3xl md:text-5xl drop-shadow-lg">
                 AI Based Poem Generator
               </h1>
-              <p className="text-white drop-shadow-lg text-xl">
+              <p className="text-white drop-shadow-lg text-lg md:text-xl">
                 Experience the magic of poetry in every line: <br />
                 Let our AI poet captivate your imagination.
               </p>
-              <div>
+              <div className="flex flex-row justify-center md:justify-start">
                 <Link href="/generate">
                   <Button color="green" size="lg">
                     Generate a poem now
