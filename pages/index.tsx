e203d6ced97430d729ac887ebdbb4b0b4b676353
RaduTek <radutek13@gmail.com>
2023-04-01 21:52:38 +0300

    Remove generation form from start page, add hero image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,74 +1,49 @@
+/* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next";
 import { Alert, Button } from "flowbite-react";
 import MainNavbar from "../components/navbar";
 import MainPage from "../components/page";
-import InputCard from "../components/inputcard";
-import PoemCard from "../components/poemcard";
-import LogInCard from "../components/LogInCard";
 import Waves from "../components/waves";
-import { useAtomValue, useAtom } from "jotai";
-import {
-  loadingPoemAtom,
-  poemShowAtom,
-  poemImageAtom,
-  poemTextAtom,
-  requestErrorAtom,
-} from "../lib/atoms";
 import HeroBanner from "../components/herobanner";
 import MainFooter from "../components/footer";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-  const requestError = useAtomValue(requestErrorAtom);
-  const [poemShow, setPoemShow] = useAtom(poemShowAtom);
-  const poemText = useAtomValue(poemTextAtom);
-  const poemImage = useAtomValue(poemImageAtom);
-  const loadingPoem = useAtomValue(loadingPoemAtom);
-
   return (
     <>
-      <LogInCard></LogInCard>
-      <Waves hue={280} height="500px" animate={loadingPoem} />
+      <Waves hue={280} height="700px" />
       <MainNavbar />
       <MainPage>
         <HeroBanner>
-          <h1 className="text-white text-center text-4xl mb-4 drop-shadow-lg">
-            AI Based Poem Generator
-          </h1>
-          <p className="text-white text-center drop-shadow-lg">
-            Unleash the power of words with AI: <br />
-            Let our poetry generator paint your world with verse.
-          </p>
-        </HeroBanner>
-        <InputCard />
-        {requestError && (
-          <Alert color="failure" className="mb-4 mx-auto shadow-md">
-            <span>
-              <span className="font-medium">Info alert!</span> Change a few
-              things up and try submitting again.
-            </span>
-          </Alert>
-        )}
-        {!requestError && poemShow && (
-          <div
-            className={
-              !poemShow
-                ? "transition duration-700 scale-y-0 opacity-0 -translate-y-1/2 h-0"
-                : "transition duration-700"
-            }
-          >
-            <PoemCard
-              title="My poem"
-              image={poemImage}
-              showOptions={true}
-              text={poemText}
-            />
-            <div className="flex flex-row gap-2 justify-center">
-              <Button color="gray" size="xs" onClick={() => setPoemShow(false)}>
-                Generate another poem
-              </Button>
+          <div className="flex flex-row h-[450px]">
+            <div
+              className="flex-1 flex items-center justify-center bg-contain bg-no-repeat bg-center"
+              style={{ backgroundImage: "url(hero_bg.png)" }}
+            >
+              <img
+                src="hero_image.png"
+                alt="Hero Image"
+                className="w-1/2 mt-14"
+              />
+            </div>
+            <div className="flex-1 flex flex-col gap-4 justify-center">
+              <h1 className="text-white text-5xl drop-shadow-lg">
+                AI Based Poem Generator
+              </h1>
+              <p className="text-white drop-shadow-lg text-xl">
+                Experience the magic of poetry in every line: <br />
+                Let our AI poet captivate your imagination.
+              </p>
+              <div>
+                <Link href="/generate">
+                  <Button color="green" size="lg">
+                    Generate a poem now
+                  </Button>
+                </Link>
+              </div>
             </div>
           </div>
-        )}
+        </HeroBanner>
       </MainPage>
       <MainFooter />
     </>
