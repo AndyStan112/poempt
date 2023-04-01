bfc15241d567507b948699ce17ddab8d7d2d7edc
RaduTek <radutek13@gmail.com>
2023-04-01 07:59:22 +0300

    Constants for forms

---
+++ utils/constants.tsx
@@ -0,0 +1,19 @@
+export const writingStyle = [
+  "Modernist",
+  "Traditionalist",
+  "Classicism",
+  "Romantic",
+  "Symbolism",
+  "Contemporary",
+];
+
+export const stanzaStyles = [
+  "Free verse",
+  "Haiku",
+  "Sonnet",
+  "Acrostic",
+  "Villanelle",
+  "Ode",
+  "Elegy",
+  "Ballad",
+];
