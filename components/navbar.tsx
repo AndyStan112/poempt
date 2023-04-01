d610b9fa867d2a90ea45f2c33497d27da7d79dc5
RaduTek <radutek13@gmail.com>
2023-04-01 21:50:04 +0300

    Update navbar links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,27 +1,36 @@
 import { Navbar, Button, DarkThemeToggle, Flowbite } from "flowbite-react";
+import Link from "next/link";
 
 function MainNavbar() {
   return (
     <>
       <Navbar className="sticky !bg-transparent !text-white drop-shadow-md">
-        <Navbar.Brand href="/" className="pr-16">
-          <span className="self-center whitespace-nowrap text-xl font-semibold ">
+        <Navbar.Brand className="pr-8">
+          <Link
+            href="/"
+            className="self-center whitespace-nowrap text-3xl font-semibold "
+          >
             PoemPT
-          </span>
+          </Link>
         </Navbar.Brand>
         <div className="flex md:order-2">
-          <Button color="green">Generate a poem</Button>
+          <Link href="/generate">
+            <Button color="green">Generate a poem</Button>
+          </Link>
           <Navbar.Toggle />
         </div>
         <Navbar.Collapse>
-          <Navbar.Link className="!text-white" href="/">
-            Home
+          <Navbar.Link className="!text-white">
+            <Link href="/">Home</Link>
           </Navbar.Link>
-          <Navbar.Link className="!text-white" href="/about">
-            About
+          <Navbar.Link className="!text-white">
+            <Link href="/library">Public library</Link>
           </Navbar.Link>
-          <Navbar.Link className="!text-white" href="/test">
-            Testing
+          <Navbar.Link className="!text-white">
+            <Link href="/mylibrary">My library</Link>
+          </Navbar.Link>
+          <Navbar.Link className="!text-white">
+            <Link href="/about">About</Link>
           </Navbar.Link>
         </Navbar.Collapse>
       </Navbar>
