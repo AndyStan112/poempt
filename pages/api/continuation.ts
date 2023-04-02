b56063cafe1ac5ff7c93bbe6d42a85f6cee79a24
AndyStan112 <stanica.andrei@gmail.com>
2023-04-02 08:24:45 +0300

    change file placement and add continue route

---
+++ pages/api/continuation.ts
@@ -0,0 +1,52 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Configuration, OpenAIApi } from 'openai';
+import keyword_extractor from 'keyword-extractor';
+const configuration = new Configuration({
+  apiKey: process.env.GPT,
+});
+const openai = new OpenAIApi(configuration);
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  try {
+    let prompt = 'make a poem';
+    const data = req.body;
+    prompt =
+      'Write the continuation of a poem. Make a sugestive title separated by spaces. Base your creation on the following poem : /n' +
+      data.poem;
+    ('.');
+
+    console.log(prompt);
+    const poemCompletion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: prompt,
+      temperature: 0.5,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      max_tokens: 128,
+    });
+    const response = poemCompletion.data.choices[0].text || '';
+    const title = response
+      .trim()
+      .split('\n')[0]
+      .replace('title', '')
+      .replace('Title', '')
+      .replace(':', '')
+      .replace('"', '');
+    const verseArray = response
+      .trim()
+      .split('\n')
+      .filter((e) => !e.toLowerCase().startsWith('verse'));
+    verseArray.splice(0, 1);
+    const poem = verseArray.join('\n');
+
+    console.log(title, poem);
+    res.status(200).send({ title: title, poem: poem });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ result: 'error' });
+  }
+}
