27a9183d41b5e19f661bf1ab8ec0a87a28b2864d
RaduTek <radutek13@gmail.com>
2023-04-02 07:03:42 +0300

    Add working login button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,12 @@
 import { Navbar, Button } from "flowbite-react";
 import Link from "next/link";
+import LoginModal from "./loginmodal";
+import { useSetAtom } from "jotai";
+import { showLoginModalAtom } from "../lib/atoms";
 
 function MainNavbar() {
+  const setShowLoginModal = useSetAtom(showLoginModalAtom);
+
   return (
     <>
       <Navbar className="sticky !bg-transparent !text-white drop-shadow-md">
@@ -11,10 +16,19 @@ function MainNavbar() {
         >
           PoemPT
         </Link>
-        <div className="flex md:order-2">
+        <div className="flex md:order-2 gap-2">
           <Link href="/generate">
-            <Button color="green">Generate a poem</Button>
+            <Button color="gray" size="sm">
+              Generate a poem
+            </Button>
           </Link>
+          <Button
+            color="gray"
+            size="sm"
+            onClick={() => setShowLoginModal(true)}
+          >
+            Login
+          </Button>
           <Navbar.Toggle />
         </div>
         <Navbar.Collapse>
@@ -32,6 +46,7 @@ function MainNavbar() {
           </Link>
         </Navbar.Collapse>
       </Navbar>
+      <LoginModal />
     </>
   );
 }
