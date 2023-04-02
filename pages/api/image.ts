99ecaad7e7078f8322cc9cc0d4c781e281c2e7f0
RaduTek <radutek13@gmail.com>
2023-04-02 09:50:04 +0300

    Add strip diacritics function

--- pages/api/image.ts
+++ pages/api/image.ts
@@ -1,7 +1,7 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { Configuration, OpenAIApi } from 'openai';
-import axios from 'axios';
-import { PrismaClient } from '@prisma/client';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
+import axios from "axios";
+import { PrismaClient } from "@prisma/client";
 const configuration = new Configuration({
   apiKey: process.env.GPT,
 });
@@ -10,19 +10,22 @@ const prisma = new PrismaClient();
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse,
+  res: NextApiResponse
 ) {
   try {
+    const str = req.body.poem;
+    const poem = str.normalize("NFKD").replace(/[^\w]/g, "");
+
     const imageCompletion = await openai.createImage({
-      prompt: req.body.poem,
+      prompt: poem,
       n: 1,
-      size: '256x256',
+      size: "256x256",
     });
-    const image = imageCompletion.data.data[0].url || '';
+    const image = imageCompletion.data.data[0].url || "";
 
     res.status(200).send({ image: image });
   } catch (error) {
     console.log(error);
-    res.status(500).send({ result: 'error' });
+    res.status(500).send({ result: "error" });
   }
 }
