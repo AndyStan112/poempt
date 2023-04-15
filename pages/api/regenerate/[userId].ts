59a242ed56f2e441cb646ec300596404a6934dd7
AndyStan112 <stanica.andrei@gmail.com>
2023-04-15 22:06:47 +0300

    clean backend code

--- pages/api/regenerate/[userId].ts
+++ pages/api/regenerate/[userId].ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-
+// import fetch from 'node-fetch';
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
@@ -8,6 +8,17 @@ export default async function handler(
     const userId = req.query.userId;
     const { poem, poemId } = req.body;
     console.log(userId, poem, poemId);
+    await fetch(process.env.NEXTAUTH_URL + '/api/image', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ poem }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
     res.status(200).send({ test: 'test' });
   } catch (error) {
     console.log('regenerate', error);
