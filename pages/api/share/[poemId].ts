d30424756335e35a3e32f84b1ebca21ef17be5b3
AndyStan112 <stanica.andrei28@gmail.com>
2023-04-16 13:35:22 +0300

    add creator data to share route rsponse

--- pages/api/share/[poemId].ts
+++ pages/api/share/[poemId].ts
@@ -1,15 +1,18 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { default as prisma } from '../../../lib/prismadb';
+import { NextApiRequest, NextApiResponse } from "next";
+import { default as prisma } from "../../../lib/prismadb";
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse,
+  res: NextApiResponse
 ) {
   try {
     const poemId = req.query.poemId as string;
-    const poem = await prisma.poem.findUnique({ where: { id: poemId } });
+    const poem = await prisma.poem.findUnique({
+      where: { id: poemId },
+      include: { creator: true },
+    });
     res.status(200).send({ poem: poem });
   } catch (error) {
     console.log(error);
-    res.status(500).send({ result: 'error' });
+    res.status(500).send({ result: "error" });
   }
 }
