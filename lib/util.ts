c6716efce06e9064e37c8a9921ffbd14cccd520f
RaduTek <radutek13@gmail.com>
2023-04-16 17:52:10 +0300

    Improve code readability

--- lib/util.ts
+++ lib/util.ts
@@ -1,8 +1,11 @@
 import { Configuration, OpenAIApi } from "openai";
+
 const configuration = new Configuration({
   apiKey: process.env.GPT,
 });
+
 const openai = new OpenAIApi(configuration);
+
 export const extractIdFromUrl = (url: string | undefined): string | null => {
   if (!url) return null;
   if (!url.startsWith("https://storage.googleapis.com/")) return null;
@@ -12,6 +15,7 @@ export const extractIdFromUrl = (url: string | undefined): string | null => {
 
   return components[components.length - 1];
 };
+
 export const generateUrlFromId = (fileName: string): string =>
   "https://storage.googleapis.com/" + process.env.BUCKET_NAME! + "/" + fileName;
 
@@ -19,12 +23,15 @@ export const sleep = (ms: number = 1000) =>
   new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
+
 export const isEmpty = (obj: Record<string, any>): boolean =>
   Object.keys(obj).length === 0 && obj.constructor === Object;
+
 type PoemCompletionResponse = {
   model: "text-davinci-003" | "gpt-3.5-turbo";
   poem: string;
 };
+
 export const getPoemCompletion = async (
   model: string,
   prompt: string
