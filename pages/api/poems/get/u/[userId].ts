3feda147da458597c55ef21aa20ca4f63f300243
AndyStan112 <stanica.andrei28@gmail.com>
2023-04-16 16:35:29 +0300

    fix private library order and take

--- pages/api/poems/get/u/[userId].ts
+++ pages/api/poems/get/u/[userId].ts
@@ -1,20 +1,21 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { default as prisma } from '../../../../../lib/prismadb';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { default as prisma } from "../../../../../lib/prismadb";
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse,
+  res: NextApiResponse
 ) {
   try {
     const userId = req.query.userId as string;
     const poems = await prisma.poem.findMany({
       where: { creatorId: userId },
       include: { creator: true },
-      take: 10,
+      take: 5,
+      orderBy: { createdAt: "desc" },
     });
 
     res.status(200).send({ poems: poems });
   } catch (error) {
     console.log(error);
-    res.status(500).send({ result: 'error' });
+    res.status(500).send({ result: "error" });
   }
 }
