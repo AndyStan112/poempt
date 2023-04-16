b9683b394c86d4e504c8b5f0595c1c04856dde0a
AndyStan112 <stanica.andrei28@gmail.com>
2023-04-16 15:23:38 +0300

    add option to choose model in backend

--- pages/api/generate/poem.ts
+++ pages/api/generate/poem.ts
@@ -1,88 +1,116 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { Configuration, OpenAIApi } from 'openai';
-import keyword_extractor from 'keyword-extractor';
-import { stanzaStyles } from '../../../lib/constants';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
+import keyword_extractor from "keyword-extractor";
+import { stanzaStyles } from "../../../lib/constants";
+type PoemCompletionResponse = {
+  model: "text-davinci-003" | "gpt-3.5-turbo";
+  poem: string;
+};
 const configuration = new Configuration({
   apiKey: process.env.GPT,
 });
 const openai = new OpenAIApi(configuration);
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  try {
-    let prompt = 'make a poem';
-    const data = req.body;
-    console.log(data);
-    const keywords = keyword_extractor
-      .extract(data.subject, {
-        language: 'english',
-        remove_digits: true,
-        return_changed_case: false,
-        remove_duplicates: true,
-      })
-      .join(',');
-
-    if (data.stanzaStyle == stanzaStyles[0]) {
-      prompt =
-        'Write a title and verses for a ' +
-        data.mood +
-        ' ' +
-        data.stanzaStyle +
-        ' poem in the ' +
-        data.writingStyle +
-        ' style, with a ' +
-        data.rhyme +
-        ' and ' +
-        data.verses +
-        ' lines per stanza, based on the following keywords: ' +
-        keywords +
-        ' .';
-    } else {
-      prompt =
-        'Write a title and verses for a ' +
-        data.mood +
-        ' ' +
-        data.stanzaStyle +
-        ' poem in the ' +
-        data.writingStyle +
-        ' style, based on the following keywords: ' +
-        data.keywords +
-        '.';
-    }
-
-    console.log('Prompt: "' + prompt + '"');
-
+const getPoemCompletion = async (
+  model: string,
+  prompt: string
+): Promise<string> => {
+  if (model === "gpt-3.5-turbo") {
     const poemCompletion = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: prompt }],
+      model,
+      messages: [{ role: "user", content: prompt }],
       temperature: 0.5,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
       max_tokens: 256,
     });
-    const response = poemCompletion.data.choices[0].message?.content || '';
 
-    console.log('Response: "' + response + '"');
+    return poemCompletion.data.choices[0].message?.content || "";
+  } else {
+    const poemCompletion = await openai.createCompletion({
+      model: model,
+      prompt: prompt,
+      temperature: 0.5,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      max_tokens: 256,
+    });
 
-    const title = response
-      .replace(/(\btitle\b|\btitlu\b|:|")/gi, '')
+    return poemCompletion.data.choices[0].text || "";
+  }
+};
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    let prompt = "make a poem";
+    const data = req.body;
+    console.log(data);
+    const keywords = keyword_extractor
+      .extract(data.subject, {
+        language: "english",
+        remove_digits: true,
+        return_changed_case: false,
+        remove_duplicates: true,
+      })
+      .join(",");
+
+    if (data.stanzaStyle == stanzaStyles[0]) {
+      prompt =
+        "Write a title and verses for a " +
+        data.mood +
+        " " +
+        data.stanzaStyle +
+        " poem in the " +
+        data.writingStyle +
+        " style, with a " +
+        data.rhyme +
+        " and " +
+        data.verses +
+        " lines per stanza, based on the following keywords: " +
+        keywords +
+        " .";
+    } else {
+      prompt =
+        "Write a title and verses for a " +
+        data.mood +
+        " " +
+        data.stanzaStyle +
+        " poem in the " +
+        data.writingStyle +
+        " style, based on the following keywords: " +
+        data.keywords +
+        ".";
+    }
+
+    console.log('Prompt: "' + prompt + '"');
+
+    const poemCompletion = await getPoemCompletion(
+      data.model || "gpt-3.5-turbo",
+      prompt
+    );
+
+    console.log('Response: "' + poemCompletion + '"');
+
+    const title = poemCompletion
+      .replace(/(\btitle\b|\btitlu\b|:|")/gi, "")
       .trim()
-      .split('\n')[0];
+      .split("\n")[0];
 
-    const verseArray = response
+    const verseArray = poemCompletion
       .trim()
-      .split('\n')
-      .filter((e) => !e.toLowerCase().startsWith('verse'));
+      .split("\n")
+      .filter((e) => !e.toLowerCase().startsWith("verse"));
     verseArray.splice(0, 1);
-    const poem = verseArray.join('\n');
+    const poem = verseArray.join("\n");
 
     console.log(title, poem);
     res.status(200).send({ title: title, poem: poem });
   } catch (error) {
     console.log(error);
-    res.status(500).send({ result: 'error' });
+    res.status(500).send({ result: "error" });
   }
 }
