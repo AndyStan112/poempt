61998ee2c72201f1841e02594ddc9dd04cec03cd
RaduTek <radutek13@gmail.com>
2023-04-16 12:11:00 +0300

    Remove unused atoms

--- lib/atoms.ts
+++ lib/atoms.ts
@@ -1,14 +1,5 @@
-import { atom } from "jotai";
+import { atom } from 'jotai';
 
 export const showLoginModalAtom = atom(false);
 
 export const loadingPoemAtom = atom(false);
-export const loadingImageAtom = atom(false);
-
-export const requestErrorAtom = atom(false);
-
-export const poemShowAtom = atom(false);
-export const poemIdAtom = atom("");
-export const poemTitleAtom = atom("");
-export const poemTextAtom = atom("");
-export const poemImageAtom = atom("loader.gif");
