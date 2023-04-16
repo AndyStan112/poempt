3ec65a2fd1e576f0ec9e2ec8a4a7193d3527fdb6
AndyStan112 <stanica.andrei28@gmail.com>
2023-04-16 13:01:25 +0300

    make backend share route return poem

--- pages/api/share/[poemId].ts
+++ pages/api/share/[poemId].ts
@@ -1,12 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { default as prisma } from '../../../lib/prismadb';
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   try {
-    const url = req.query.poemId;
-
-    res.status(200).send({ t: 'downloadUrl' });
+    const poemId = req.query.poemId as string;
+    const poem = await prisma.poem.findUnique({ where: { id: poemId } });
+    res.status(200).send({ poem: poem });
   } catch (error) {
     console.log(error);
     res.status(500).send({ result: 'error' });
