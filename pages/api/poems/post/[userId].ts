470f016ab91740e796a7a2064b20b626e43e499d
AndyStan112 <stanica.andrei28@gmail.com>
2023-06-14 21:45:08 +0300

    test file creation

--- pages/api/poems/post/[userId].ts
+++ pages/api/poems/post/[userId].ts
@@ -1,33 +1,33 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { default as prisma } from '../../../../lib/prismadb';
-import fetch from 'node-fetch';
-import { v4 as uuidv4 } from 'uuid';
-import { GLOBAL_TAKE } from '../../../../lib/constants';
-import { extractIdFromUrl } from '../../../../lib/util';
-import bucket from '../../../../lib/bucket';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { default as prisma } from "../../../../lib/prismadb";
+import fetch from "node-fetch";
+import { v4 as uuidv4 } from "uuid";
+import { GLOBAL_TAKE } from "../../../../lib/constants";
+import { extractIdFromUrl } from "../../../../lib/util";
+import bucket from "../../../../lib/bucket";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse,
+  res: NextApiResponse
 ) {
   try {
-    if (req.method != 'POST')
-      res.status(500).send({ message: 'Wrong request' });
+    if (req.method != "POST")
+      res.status(500).send({ message: "Wrong request" });
     const userId = req.query.userId as string;
     const { title, poem, image } = req.body;
 
-    const fileName = uuidv4() + '.png';
+    const fileName = uuidv4() + ".png";
     const file = bucket.file(fileName);
-    const writeStream = file.createWriteStream();
+    // const writeStream = file.createWriteStream();
 
-    await fetch(image).then((res: any) => {
-      res.body.pipe(writeStream);
-    });
+    // await fetch(image).then((res: any) => {
+    //   res.body.pipe(writeStream);
+    // });
 
     const imageUrl =
-      'https://storage.googleapis.com/' +
+      "https://storage.googleapis.com/" +
       process.env.BUCKET_NAME! +
-      '/' +
+      "/" +
       fileName;
     const poemPost = await prisma.poem.create({
       data: { creatorId: userId, title: title, poem: poem, image: imageUrl },
@@ -36,17 +36,17 @@ export default async function handler(
       //get Latest Poems
       const latestPoems = await prisma.poem.findMany({
         where: { creatorId: userId },
-        orderBy: { createdAt: 'desc' },
+        orderBy: { createdAt: "desc" },
         select: { id: true },
         take: GLOBAL_TAKE,
       });
-      console.log('latest: ', latestPoems);
+      console.log("latest: ", latestPoems);
       //get Bookmarked Poems
       const bookmarkedPoemIds = await prisma.bookmark.findMany({
         where: { saverId: userId },
         select: { poemId: true },
       });
-      console.log('bookmarked: ', bookmarkedPoemIds);
+      console.log("bookmarked: ", bookmarkedPoemIds);
       //get poems to be deleted
       const deletables = await prisma.poem.findMany({
         where: {
@@ -70,11 +70,11 @@ export default async function handler(
         },
       });
       if (!deletables.length) {
-        res.status(200).send({ result: 'successful', poemId: poemPost.id });
+        res.status(200).send({ result: "successful", poemId: poemPost.id });
         return;
       }
 
-      console.log('Will delete : ', deletables);
+      console.log("Will delete : ", deletables);
       const deleteCount = await prisma.poem.deleteMany({
         where: {
           AND: [
@@ -83,7 +83,7 @@ export default async function handler(
           ],
         },
       });
-      console.log('count: ', deleteCount);
+      console.log("count: ", deleteCount);
 
       deletables
         .map(({ image }) => extractIdFromUrl(image))
@@ -91,12 +91,12 @@ export default async function handler(
           if (id) bucket.file(id).delete();
         });
     } catch (e) {
-      console.log('post poems', e);
+      console.log("post poems", e);
     }
-    console.log('poemPost.id: ' + poemPost.id);
-    res.status(200).send({ result: 'successful', poemId: poemPost.id });
+    console.log("poemPost.id: " + poemPost.id);
+    res.status(200).send({ result: "successful", poemId: poemPost.id });
   } catch (error) {
     console.log(error);
-    res.status(500).send({ result: 'error' });
+    res.status(500).send({ result: "error" });
   }
 }
